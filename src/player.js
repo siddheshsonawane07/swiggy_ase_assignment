8a93cbe284f9b383653f9dab4f84fba369b07e50
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-06-28 20:11:40 +0530

    feat: player class

---
+++ src/player.js
@@ -0,0 +1,11 @@
+class Player {
+  constructor(id, name, health, strength, attack) {
+    this.id = id;
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    this.attack = attack;
+  }
+}
+
+export default Player;
